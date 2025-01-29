e1cc97dc3b76b7fba6f5ed4a5c59dff3c5caf1ce
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2025-01-29 19:13:12 +1300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,323 @@
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+
+mod parse;
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum Chara {
+    #[serde(rename = "MIK")]
+    Miku,
+    #[serde(rename = "RIN")]
+    Rin,
+    #[serde(rename = "LEN")]
+    Len,
+    #[serde(rename = "LUK")]
+    Luka,
+    #[serde(rename = "NER")]
+    Neru,
+    #[serde(rename = "HAK")]
+    Haku,
+    #[serde(rename = "KAI")]
+    Kaito,
+    #[serde(rename = "MEI")]
+    Meiko,
+    #[serde(rename = "SAK")]
+    Sakine,
+    #[serde(rename = "TET")]
+    Teto,
+    #[serde(rename = "EXT")]
+    Extra,
+    #[serde(rename = "ALL")]
+    All,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ItemPart {
+    Kami,
+    Face,
+    Neck,
+    Zujo,
+    Back,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Costume {
+    pub id: i32,
+    pub items: Vec<i32>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Module {
+    pub cos: Costume,
+    pub chara: Chara,
+    pub name: Option<String>,
+    pub name_jp: Option<String>,
+    pub name_en: Option<String>,
+    pub name_cn: Option<String>,
+    pub name_fr: Option<String>,
+    pub name_ge: Option<String>,
+    pub name_it: Option<String>,
+    pub name_kr: Option<String>,
+    pub name_sp: Option<String>,
+    pub name_tw: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Customize {
+    pub bind_module: Option<i32>,
+    pub chara: Chara,
+    pub part: ItemPart,
+    pub name: Option<String>,
+    pub name_jp: Option<String>,
+    pub name_en: Option<String>,
+    pub name_cn: Option<String>,
+    pub name_fr: Option<String>,
+    pub name_ge: Option<String>,
+    pub name_it: Option<String>,
+    pub name_kr: Option<String>,
+    pub name_sp: Option<String>,
+    pub name_tw: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct ModuleDb {
+    pub modules: BTreeMap<i32, Module>,
+    pub cstm_items: BTreeMap<i32, Customize>,
+}
+
+impl ModuleDb {
+    pub async fn from_files<P: AsRef<std::path::Path>>(
+        gm_module_tbl: Option<P>,
+        gm_customize_item_tbl: Option<P>,
+        chritm_prop: Option<P>,
+        mod_str_array: Option<P>,
+    ) -> Option<Self> {
+        let mut module_db = Self {
+            modules: BTreeMap::new(),
+            cstm_items: BTreeMap::new(),
+        };
+
+        if let Some(gm_module_tbl) = gm_module_tbl {
+            let gm_module_tbl = parse::Module::parse(gm_module_tbl).await?;
+            for module in gm_module_tbl.data {
+                let cos = module.cos.trim_start_matches("COS_").parse::<i32>().ok()? - 1;
+                module_db.modules.insert(
+                    module.id,
+                    Module {
+                        cos: Costume {
+                            id: cos,
+                            items: vec![],
+                        },
+                        chara: module.chara,
+                        name: None,
+                        name_jp: Some(module.name.clone()),
+                        name_en: None,
+                        name_cn: None,
+                        name_fr: None,
+                        name_ge: None,
+                        name_it: None,
+                        name_kr: None,
+                        name_sp: None,
+                        name_tw: None,
+                    },
+                );
+            }
+        }
+
+        if let Some(gm_customize_item_tbl) = gm_customize_item_tbl {
+            let gm_customize_item_tbl = parse::CstmItem::parse(gm_customize_item_tbl).await?;
+            for cstm_item in gm_customize_item_tbl.data {
+                module_db.cstm_items.insert(
+                    cstm_item.id,
+                    Customize {
+                        bind_module: cstm_item.bind_module,
+                        chara: cstm_item.chara,
+                        part: cstm_item.parts,
+                        name: None,
+                        name_jp: Some(cstm_item.name.clone()),
+                        name_en: None,
+                        name_cn: None,
+                        name_fr: None,
+                        name_ge: None,
+                        name_it: None,
+                        name_kr: None,
+                        name_sp: None,
+                        name_tw: None,
+                    },
+                );
+            }
+        }
+
+        if let Some(chritm_prop) = chritm_prop {
+            let chritm_prop = parse::Costume::parse(chritm_prop).await?;
+            for (_, module) in &mut module_db.modules {
+                if let Some(prop) = chritm_prop.get(&module.chara) {
+                    if let Some(cos) = prop
+                        .data
+                        .iter()
+                        .filter(|cos| cos.id == module.cos.id)
+                        .next()
+                    {
+                        module.cos.items = cos.item.clone();
+                    }
+                }
+            }
+        }
+
+        if let Some(mod_str_array) = mod_str_array {
+            let mod_str_array = parse::ModStringArray::parse(mod_str_array).await?;
+            for (id, module) in &mut module_db.modules {
+                if let Some(data) = &mod_str_array.data {
+                    if let Some(modules) = &data.module {
+                        module.name = modules.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.en {
+                    if let Some(modules) = &data.module {
+                        module.name_en = modules.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.cn {
+                    if let Some(modules) = &data.module {
+                        module.name_cn = modules.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.fr {
+                    if let Some(modules) = &data.module {
+                        module.name_fr = modules.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.ge {
+                    if let Some(modules) = &data.module {
+                        module.name_ge = modules.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.it {
+                    if let Some(modules) = &data.module {
+                        module.name_it = modules.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.kr {
+                    if let Some(modules) = &data.module {
+                        module.name_kr = modules.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.sp {
+                    if let Some(modules) = &data.module {
+                        module.name_sp = modules.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.tw {
+                    if let Some(modules) = &data.module {
+                        module.name_tw = modules.get(id).cloned();
+                    }
+                }
+            }
+            for (id, cstm_item) in &mut module_db.cstm_items {
+                if let Some(data) = &mod_str_array.data {
+                    if let Some(customize) = &data.customize {
+                        cstm_item.name = customize.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.en {
+                    if let Some(customize) = &data.customize {
+                        cstm_item.name_en = customize.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.cn {
+                    if let Some(customize) = &data.customize {
+                        cstm_item.name_cn = customize.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.fr {
+                    if let Some(customize) = &data.customize {
+                        cstm_item.name_fr = customize.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.ge {
+                    if let Some(customize) = &data.customize {
+                        cstm_item.name_ge = customize.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.it {
+                    if let Some(customize) = &data.customize {
+                        cstm_item.name_it = customize.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.kr {
+                    if let Some(customize) = &data.customize {
+                        cstm_item.name_kr = customize.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.sp {
+                    if let Some(customize) = &data.customize {
+                        cstm_item.name_sp = customize.get(id).cloned();
+                    }
+                }
+                if let Some(data) = &mod_str_array.tw {
+                    if let Some(customize) = &data.customize {
+                        cstm_item.name_tw = customize.get(id).cloned();
+                    }
+                }
+            }
+        }
+
+        if module_db.modules.len() == 0 && module_db.cstm_items.len() == 0 {
+            None
+        } else {
+            Some(module_db)
+        }
+    }
+
+    pub async fn from_folder<P: AsRef<std::path::Path>>(path: P) -> Option<Self> {
+        let path = path.as_ref();
+        if !path.is_dir() {
+            return None;
+        }
+
+        let module_tbl = path.join("mod_gm_module_tbl.farc");
+        let module_tbl = if module_tbl.exists() {
+            Some(module_tbl)
+        } else {
+            None
+        };
+
+        let customize_tbl = path.join("mod_gm_customize_item_tbl.farc");
+        let customize_tbl = if customize_tbl.exists() {
+            Some(customize_tbl)
+        } else {
+            None
+        };
+
+        let chritm_prop = path.join("mod_chritm_prop.farc");
+        let chritm_prop = if chritm_prop.exists() {
+            Some(chritm_prop)
+        } else {
+            None
+        };
+
+        let mod_str_array = path.join("lang2").join("mod_str_array.toml");
+        let mod_str_array = if mod_str_array.exists() {
+            Some(mod_str_array)
+        } else {
+            None
+        };
+
+        Self::from_files(module_tbl, customize_tbl, chritm_prop, mod_str_array).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn read_mod() {
+        let db = ModuleDb::from_folder("/home/brogamer/Downloads/Interviewer Rin/rom")
+            .await
+            .unwrap();
+        dbg!(db);
+    }
+}
---
+++ src/parse.rs
@@ -0,0 +1,215 @@
+use itertools::Itertools;
+use serde::de::{MapAccess, Visitor};
+use serde::Deserialize;
+use serde_with::{serde_as, DisplayFromStr};
+use std::collections::BTreeMap;
+
+#[derive(Deserialize, Clone)]
+pub struct Module {
+    pub chara: crate::Chara,
+    pub cos: String,
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct Costume {
+    pub id: i32,
+    pub item: Vec<i32>,
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct CstmItem {
+    pub bind_module: Option<i32>,
+    pub chara: crate::Chara,
+    pub id: i32,
+    pub name: String,
+    pub parts: crate::ItemPart,
+}
+
+#[derive(Clone, Debug)]
+pub struct DivaTbl<T> {
+    pub data: Vec<T>,
+}
+
+struct DivaMapVisitor<T> {
+    marker: std::marker::PhantomData<fn() -> T>,
+}
+
+impl<T> DivaMapVisitor<T> {
+    fn new() -> Self {
+        Self {
+            marker: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<'de, T: Deserialize<'de>> Visitor<'de> for DivaMapVisitor<T> {
+    type Value = DivaTbl<T>;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("module")
+    }
+
+    fn visit_map<M: MapAccess<'de>>(self, mut access: M) -> Result<Self::Value, M::Error> {
+        let mut vec = Vec::new();
+        loop {
+            let entry = access.next_entry::<i32, T>();
+            let Ok(entry) = entry else {
+                continue;
+            };
+            let Some((_, value)) = entry else {
+                break;
+            };
+            vec.push(value);
+        }
+
+        Ok(Self::Value { data: vec })
+    }
+}
+
+impl<'de, T: Deserialize<'de>> Deserialize<'de> for DivaTbl<T> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_map(DivaMapVisitor::new())
+    }
+}
+
+pub fn clean_input(input: &str) -> String {
+    input
+        .lines()
+        .dedup()
+        .filter(|line| line.contains('='))
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+
+impl Module {
+    pub async fn parse<P: AsRef<std::path::Path>>(path: P) -> Option<DivaTbl<Self>> {
+        let path = path.as_ref();
+        if !path.exists() {
+            return None;
+        }
+
+        let str = path.to_str()?;
+        let contents = if str.ends_with("gm_module_tbl.farc") {
+            let farc = farc::Farc::from_file(path).ok()?;
+            let file = farc.entries.get("gm_module_id.bin")?;
+            let buf = file.to_buf_const()?;
+            String::from_utf8(buf.to_vec()).ok()?
+        } else if str.ends_with("gm_module_id.bin") {
+            tokio::fs::read_to_string(path).await.ok()?
+        } else {
+            return None;
+        };
+
+        serde_divatree::from_str(&clean_input(&contents)).ok()
+    }
+}
+
+impl Costume {
+    pub async fn parse<P: AsRef<std::path::Path>>(
+        path: P,
+    ) -> Option<BTreeMap<crate::Chara, DivaTbl<Self>>> {
+        let path = path.as_ref();
+        if !path.exists() {
+            return None;
+        }
+        if !path.to_str()?.ends_with("chritm_prop.farc") {
+            return None;
+        }
+
+        let mut map = BTreeMap::new();
+        let farc = farc::Farc::from_file(path).ok()?;
+        for (name, data) in farc.entries {
+            if !name.ends_with("itm_tbl.txt") {
+                continue;
+            }
+            let chara = match name.trim_end_matches("itm_tbl.txt") {
+                "mik" => crate::Chara::Miku,
+                "rin" => crate::Chara::Rin,
+                "len" => crate::Chara::Len,
+                "luk" => crate::Chara::Luka,
+                "ner" => crate::Chara::Neru,
+                "hak" => crate::Chara::Haku,
+                "kai" => crate::Chara::Kaito,
+                "mei" => crate::Chara::Meiko,
+                "sak" => crate::Chara::Sakine,
+                "tet" => crate::Chara::Teto,
+                "ext" => crate::Chara::Extra,
+                _ => continue,
+            };
+            let buf = data.to_buf_const()?;
+            let data = String::from_utf8(buf.to_vec()).ok()?;
+            let data = serde_divatree::from_str(&clean_input(&data)).ok()?;
+            map.insert(chara, data);
+        }
+
+        if map.len() == 0 {
+            None
+        } else {
+            Some(map)
+        }
+    }
+}
+
+impl CstmItem {
+    pub async fn parse<P: AsRef<std::path::Path>>(path: P) -> Option<DivaTbl<Self>> {
+        let path = path.as_ref();
+        if !path.exists() {
+            return None;
+        }
+
+        let str = path.to_str()?;
+        let contents = if str.ends_with("gm_customize_item_tbl.farc") {
+            let farc = farc::Farc::from_file(path).ok()?;
+            let file = farc.entries.get("gm_customize_item_id.bin")?;
+            let buf = file.to_buf_const()?;
+            String::from_utf8(buf.to_vec()).ok()?
+        } else if str.ends_with("gm_customize_item_id.bin") {
+            tokio::fs::read_to_string(path).await.ok()?
+        } else {
+            return None;
+        };
+
+        serde_divatree::from_str(&clean_input(&contents)).ok()
+    }
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct ModStringArray {
+    #[serde(flatten)]
+    pub data: Option<ModStringArrayData>,
+    pub en: Option<ModStringArrayData>,
+    pub cn: Option<ModStringArrayData>,
+    pub fr: Option<ModStringArrayData>,
+    pub ge: Option<ModStringArrayData>,
+    pub it: Option<ModStringArrayData>,
+    pub kr: Option<ModStringArrayData>,
+    pub sp: Option<ModStringArrayData>,
+    pub tw: Option<ModStringArrayData>,
+}
+
+#[serde_as]
+#[derive(Deserialize, Clone, Debug)]
+pub struct ModStringArrayData {
+    #[serde_as(as = "Option<BTreeMap<DisplayFromStr, _>>")]
+    pub module: Option<BTreeMap<i32, String>>,
+    #[serde(alias = "cstm_item")]
+    #[serde_as(as = "Option<BTreeMap<DisplayFromStr, _>>")]
+    pub customize: Option<BTreeMap<i32, String>>,
+}
+
+impl ModStringArray {
+    pub async fn parse<P: AsRef<std::path::Path>>(path: P) -> Option<Self> {
+        let path = path.as_ref();
+        if !path.exists() {
+            return None;
+        }
+
+        let contents = tokio::fs::read_to_string(path).await.ok()?;
+        toml::from_str(&contents).ok()
+    }
+}
